b0231ff74f37b344f458813259c8e1ba972fe0b7
Natasha Koller <kollernatasha@gmail.com>
2021-11-21 09:23:27 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked and it felt so good!');
+}
+
+input.addEventListener('click', clickAlert);
+
 }
